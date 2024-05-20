c013144f2c102389377870d0fb45b46e091e1dec
vandeben <benjaminvanderwoerd@gmail.com>
2024-05-19 22:23:23 -0400

    wording changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
         <div className="max-w-2xl text-left h-full flex flex-col justify-between">
             <div>
               <h1 className="mb-4 text-4xl font-bold text-white">
-                <span className="text-white">Local Chef-Inspired Flavours</span>
+                <span className="text-white">Chef Developed Flavours</span>
               </h1>
               <p className="mb-4 text-white">
               <h2>Discover the delicious side of relief with OBEX, now available in three chef-inspired flavours.</h2>
