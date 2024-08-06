9e787add09224382400cece94bbbab2ebd4c18cf
vandeben <benjaminvanderwoerd@gmail.com>
2024-08-06 16:18:13 -0400

    further production mods for mailchimp

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+   /** @type {import('next').NextConfig} */
+   const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+      MAILCHIMP_SERVER_PREFIX: process.env.MAILCHIMP_SERVER_PREFIX,
+    },
+  }
 
-export default nextConfig;
+  export default nextConfig
